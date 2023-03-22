45c073215634131ddc71bcdb744eb6c2b39d6db0
apolo <apoloc43@icloud.com>
2023-03-21 23:15:02 -0300

    21/03 estilizando e criando a função detalhar

--- aluguel/urls.py
+++ aluguel/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
 """
 from django.urls import path
 from .views import index, lista_carros, realizar_aluguel, detalhar_carro, realizar_aluguel_carro,listar_clientes,listar_alugueis
+from .views import cadastrar_carro,detalhar_cliente,atualizar_carro,deletar_carro
 
 urlpatterns = [
     path('', index, name='index'),
     path('carros/', lista_carros, name='listar_carros'),
     path('alugueis/', listar_alugueis, name='listar_alugueis'),
+    path('carro/cadastrar', cadastrar_carro, name='cadastrar_carro'),
+    path('carro/atualizar/<int:pk>', atualizar_carro, name='atualizar_carro'),
     
+    
+    path('carro/deletar/<int:pk>', deletar_carro, name='deletar_carro'),
     path('cliente/', listar_clientes, name='listar_clientes'),
+    path('cliente/<int:pk>', detalhar_cliente, name='detalhar_cliente'),
     path('carros/<int:pk>', detalhar_carro, name='detalhar_carro'),
     path('carros/aluguel/<int:carro_pk>', realizar_aluguel_carro, name='realizar_aluguel_carro' ),
     path('aluguel/add', realizar_aluguel, name='realizar_aluguel'),
--- aluguel/views.py
+++ aluguel/views.py
@@ -1,22 +1,13 @@
 # Create your views here.
 from django.shortcuts import render, redirect
 from .models import Carro, Aluguel,Cliente
-from .forms import AluguelForm
+from .forms import AluguelForm,CarroForm
 
 # Create your views here.
 
 def index(request):
     carros = Carro.objects.all()[:5]
     return render(request, 'index.html', {"carros":carros})
-
-def listar_clientes(request):
-    clientes = Cliente.objects.all()
-    return render(request, 'cliente/listar.html', {"clientes":clientes})
-
-def listar_alugueis(request):
-    alugueis = Aluguel.objects.all()
-    return render(request, 'aluguel/listar.html', {"alugueis":alugueis})
-    
 
 def lista_carros(request):
     carros = Carro.objects.all()
@@ -25,6 +16,58 @@ def lista_carros(request):
 def detalhar_carro(request, pk):
     carro = Carro.objects.get(pk=pk)
     return render(request, 'carro/detalhar.html', {"carro":carro})
+
+def cadastrar_carro(request):
+    if request.method == "POST":
+        form = CarroForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            form = CarroForm()
+            return render(request, "carro/cadastrar.html" , {'form': form})
+    else:
+        form = CarroForm()
+        return render(request, "carro/cadastrar.html", {'form': form})
+
+def atualizar_carro(request, pk):
+    carro = Carro.objects.get(pk=pk)
+    form = CarroForm(instance=carro)
+    
+    if request.method == "POST":
+        form = CarroForm(request.POST, request.FILES, instance=carro)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return render(request, "carro/atualizar.html", {'form': form})
+    else:
+        return render(request, "carro/atualizar.html", {'form': form})
+    
+def deletar_carro(request, pk):
+    carro = Carro.objects.get(pk=pk)
+
+    if carro:
+        carro.delete()
+        return redirect("/")
+    else:
+        return render(request, "carro/listar.html", {'msg': "carro não encontrado"})
+
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'cliente/listar.html', {"clientes":clientes})
+
+def detalhar_cliente(request, pk):
+    cliente = Cliente.objects.get(pk=pk)
+    return render(request, 'cliente/detalhar.html', {"cliente":cliente})
+
+def listar_alugueis(request):
+    alugueis = Aluguel.objects.all()
+    return render(request, 'aluguel/listar.html', {"alugueis":alugueis})
+
+    
+
 
 def realizar_aluguel(request):
     if request.method == "POST":
@@ -57,3 +100,4 @@ def realizar_aluguel_carro(request, carro_pk):
         form = AluguelForm(instance=aluguel)
         return render(request, "aluguel/cadastrar.html", {'form': form})
     
+
